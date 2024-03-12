487df3d67fc4ecb81910c8ef713bc87fd0cc8978
jennaaaaaaxx <badleftfoot5@gmail.com>
2024-03-12 13:06:12 +0000

    Arrays
    thankfully for my code youtube exists

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,372 +8,59 @@ using UnityEngine.UI;
 
 // Unsigned 32bit int limit keeps getting reached - NEEDS FIXING ASAP!
 // Fix in the works fingers crossed :3
+// THIS IS ACTUALLY SO HARD BRO IM CLOSE TO GIVING UP!!!!!!
+// Lists save me...
+// Reset prefs at line 224
 
 public class Game : MonoBehaviour
 {
     // Clicker
     [Header("Clicker")]
     public Text scoreText;
+    
     [Header("Current Score")]
     public double currentScore;
-    [Header("Current Ones - Nonillions")]
-    public double currentOnesTensHundreds;
-    public double currentThousands;
-    public double currentMillions;
-    public double currentBillions;
-    public double currentTrillions;
-    public double currentQuadrillons;
-    public double currentQuintrillons;
-    public double currentSextillons;
-    public double currentSeptillons;
-    public double currentOctillions;
-    public double currentNonillions;
-    [Header("Current Decillions")]
-    public double currentDecillions;
-    public double currentUndecillions;
-    public double currentDuodecillions;
-    public double currentTredecillions;
-    public double currentQuaddecillions;
-    public double currentQuindecillions;
-    public double currentSexdecillions;
-    public double currentSeptdecillions;
-    public double currentOctodecillions;
-    public double currentNovemdecillions;
-    [Header("Current Vigintillions")]
-    public double currentVigintillions;
-    public double currentDuovigintillions;
-    public double currentTresvigintillions;
-    public double currentQuattorvigintillions;
-    public double currentQuinvigintillions;
-    public double currentSexvigintillions;
-    public double currentSeptenvigintillions;
-    public double currentOctovigintillions;
-    public double currentNovemvigintillions;
+    public List<double> currentScoreList = new List<double>();
 
     [Header("Total Score")]
     public double totalScore;
-    [Header("Total Ones - Nonillions")]
-    public double totalOnesTensHundreds;
-    public double totalThousands;
-    public double totalMillions;
-    public double totalBillions;
-    public double totalTrillions;
-    public double totalQuadrillons;
-    public double totalQuintrillons;
-    public double totalSextillons;
-    public double totalSeptillons;
-    public double totalOctillions;
-    public double totalNonillions;
-    [Header("Total Decillions")]
-    public double totalDecillions;
-    public double totalUndecillions;
-    public double totalDuodecillions;
-    public double totalTredecillions;
-    public double totalQuaddecillions;
-    public double totalQuindecillions;
-    public double totalSexdecillions;
-    public double totalSeptdecillions;
-    public double totalOctodecillions;
-    public double totalNovemdecillions;
-    [Header("Total Vigintillions")]
-    public double totalVigintillions;
-    public double totalDuovigintillions;
-    public double totalTresvigintillions;
-    public double totalQuattorvigintillions;
-    public double totalQuinvigintillions;
-    public double totalSexvigintillions;
-    public double totalSeptenvigintillions;
-    public double totalOctovigintillions;
-    public double totalNovemvigintillions;
+    public List<double> totalScoreList = new List<double>();
 
     [Header("Hitpower")] // KEEP THESE AS STATICS
     public static double hitPower;
-    [Header("Hitpower Ones - Nonillions")]
-    public static double hitpowerOnesTensHundreds;
-    public static double hitpowerThousands;
-    public static double hitpowerMillions;
-    public static double hitpowerBillions;
-    public static double hitpowerTrillions;
-    public static double hitpowerQuadrillons;
-    public static double hitpowerQuintrillons;
-    public static double hitpowerSextillons;
-    public static double hitpowerSeptillons;
-    public static double hitpowerOctillions;
-    public static double hitpowerNonillions;
-    [Header("Hitpower Decillions")]
-    public static double hitpowerDecillions;
-    public static double hitpowerUndecillions;
-    public static double hitpowerDuodecillions;
-    public static double hitpowerTredecillions;
-    public static double hitpowerQuaddecillions;
-    public static double hitpowerQuindecillions;
-    public static double hitpowerSexdecillions;
-    public static double hitpowerSeptdecillions;
-    public static double hitpowerOctodecillions;
-    public static double hitpowerNovemdecillions;
-    [Header("Hitpower Vigintillions")]
-    public static double hitpowerVigintillions;
-    public static double hitpowerDuovigintillions;
-    public static double hitpowerTresvigintillions;
-    public static double hitpowerQuattorvigintillions;
-    public static double hitpowerQuinvigintillions;
-    public static double hitpowerSexvigintillions;
-    public static double hitpowerSeptenvigintillions;
-    public static double hitpowerOctovigintillions;
-    public static double hitpowerNovemvigintillions;
+    public static List<double> hitPowerList = new List<double>();
+
     [Header("Score Increase")]
     public double scoreIncreasedPerSecond;
-    [Header("Increase Ones - Nonillions")]
-    public double increaseOnesTensHundreds;
-    public double increaseThousands;
-    public double increaseMillions;
-    public double increaseBillions;
-    public double increaseTrillions;
-    public double increaseQuadrillons;
-    public double increaseQuintrillons;
-    public double increaseSextillons;
-    public double increaseSeptillons;
-    public double increaseOctillions;
-    public double increaseNonillions;
-    [Header("Increase Decillions")]
-    public double increaseDecillions;
-    public double increaseUndecillions;
-    public double increaseDuodecillions;
-    public double increaseTredecillions;
-    public double increaseQuaddecillions;
-    public double increaseQuindecillions;
-    public double increaseSexdecillions;
-    public double increaseSeptdecillions;
-    public double increaseOctodecillions;
-    public double increaseNovemdecillions;
-    [Header("Increase Vigintillions")]
-    public double increaseVigintillions;
-    public double increaseDuovigintillions;
-    public double increaseTresvigintillions;
-    public double increaseQuattorvigintillions;
-    public double increaseQuinvigintillions;
-    public double increaseSexvigintillions;
-    public double increaseSeptenvigintillions;
-    public double increaseOctovigintillions;
-    public double increaseNovemvigintillions;
+    public List<double> scoreIncreaseList = new List<double>();
 
     // Shop
     [Header("Shop")]
 
     [Header("House")]
     public Text shop1Text;
-    
-    [Header("House Cost")]
     public double shop1Price;
-    
-    [Header("House Ones - Nonillions")]
-    public double shop1CostOnesTensHundreds;
-    public double shop1CostThousands;
-    public double shop1CostMillions;
-    public double shop1CostBillions;
-    public double shop1CostTrillions;
-    public double shop1CostQuadrillons;
-    public double shop1CostQuintrillons;
-    public double shop1CostSextillons;
-    public double shop1CostSeptillons;
-    public double shop1CostOctillions;
-    public double shop1CostNonillions;
-    
-    [Header("House Decillions")]
-    public double shop1CostDecillions;
-    public double shop1CostUndecillions;
-    public double shop1CostDuodecillions;
-    public double shop1CostTredecillions;
-    public double shop1CostQuaddecillions;
-    public double shop1CostQuindecillions;
-    public double shop1CostSexdecillions;
-    public double shop1CostSeptdecillions;
-    public double shop1CostOctodecillions;
-    public double shop1CostNovemdecillions;
-    
-    [Header("House Vigintillions")]
-    public double shop1CostVigintillions;
-    public double shop1CostDuovigintillions;
-    public double shop1CostTresvigintillions;
-    public double shop1CostQuattorvigintillions;
-    public double shop1CostQuinvigintillions;
-    public double shop1CostSexvigintillions;
-    public double shop1CostSeptenvigintillions;
-    public double shop1CostOctovigintillions;
-    public double shop1CostNovemvigintillions;
-
+    public List<double> shop1List = new List<double>();
 
     [Header("Tavern")]
     public Text shop2Text;
-
-    [Header("Tavern Cost")]
     public double shop2Price;
-
-    [Header("Tavern Ones - Nonillions")]
-    public double shop2CostOnesTensHundreds;
-    public double shop2CostThousands;
-    public double shop2CostMillions;
-    public double shop2CostBillions;
-    public double shop2CostTrillions;
-    public double shop2CostQuadrillons;
-    public double shop2CostQuintrillons;
-    public double shop2CostSextillons;
-    public double shop2CostSeptillons;
-    public double shop2CostOctillions;
-    public double shop2CostNonillions;
-
-    [Header("Tavern Decillions")]
-    public double shop2CostDecillions;
-    public double shop2CostUndecillions;
-    public double shop2CostDuodecillions;
-    public double shop2CostTredecillions;
-    public double shop2CostQuaddecillions;
-    public double shop2CostQuindecillions;
-    public double shop2CostSexdecillions;
-    public double shop2CostSeptdecillions;
-    public double shop2CostOctodecillions;
-    public double shop2CostNovemdecillions;
-
-    [Header("Tavern Vigintillions")]
-    public double shop2CostVigintillions;
-    public double shop2CostDuovigintillions;
-    public double shop2CostTresvigintillions;
-    public double shop2CostQuattorvigintillions;
-    public double shop2CostQuinvigintillions;
-    public double shop2CostSexvigintillions;
-    public double shop2CostSeptenvigintillions;
-    public double shop2CostOctovigintillions;
-    public double shop2CostNovemvigintillions;
-
+    public List<double> shop2List = new List<double>();
 
     [Header("Stable")]
     public Text shop3Text;
-
-    [Header("Stable Cost")]
     public double shop3Price;
-
-    [Header("Stable Ones - Nonillions")]
-    public double shop3CostOnesTensHundreds;
-    public double shop3CostThousands;
-    public double shop3CostMillions;
-    public double shop3CostBillions;
-    public double shop3CostTrillions;
-    public double shop3CostQuadrillons;
-    public double shop3CostQuintrillons;
-    public double shop3CostSextillons;
-    public double shop3CostSeptillons;
-    public double shop3CostOctillions;
-    public double shop3CostNonillions;
-
-    [Header("Stable Decillions")]
-    public double shop3CostDecillions;
-    public double shop3CostUndecillions;
-    public double shop3CostDuodecillions;
-    public double shop3CostTredecillions;
-    public double shop3CostQuaddecillions;
-    public double shop3CostQuindecillions;
-    public double shop3CostSexdecillions;
-    public double shop3CostSeptdecillions;
-    public double shop3CostOctodecillions;
-    public double shop3CostNovemdecillions;
-
-    [Header("Stable Vigintillions")]
-    public double shop3CostVigintillions;
-    public double shop3CostDuovigintillions;
-    public double shop3CostTresvigintillions;
-    public double shop3CostQuattorvigintillions;
-    public double shop3CostQuinvigintillions;
-    public double shop3CostSexvigintillions;
-    public double shop3CostSeptenvigintillions;
-    public double shop3CostOctovigintillions;
-    public double shop3CostNovemvigintillions;
-
+    public List<double> shop3List = new List<double>();
 
     [Header("Bakery")]
     public Text shop4Text;
-
-    [Header("Bakery Cost")]
     public double shop4Price;
-
-    [Header("Bakery Ones - Nonillions")]
-    public double shop4CostOnesTensHundreds;
-    public double shop4CostThousands;
-    public double shop4CostMillions;
-    public double shop4CostBillions;
-    public double shop4CostTrillions;
-    public double shop4CostQuadrillons;
-    public double shop4CostQuintrillons;
-    public double shop4CostSextillons;
-    public double shop4CostSeptillons;
-    public double shop4CostOctillions;
-    public double shop4CostNonillions;
-
-    [Header("Bakery Decillions")]
-    public double shop4CostDecillions;
-    public double shop4CostUndecillions;
-    public double shop4CostDuodecillions;
-    public double shop4CostTredecillions;
-    public double shop4CostQuaddecillions;
-    public double shop4CostQuindecillions;
-    public double shop4CostSexdecillions;
-    public double shop4CostSeptdecillions;
-    public double shop4CostOctodecillions;
-    public double shop4CostNovemdecillions;
-
-    [Header("Bakery Vigintillions")]
-    public double shop4CostVigintillions;
-    public double shop4CostDuovigintillions;
-    public double shop4CostTresvigintillions;
-    public double shop4CostQuattorvigintillions;
-    public double shop4CostQuinvigintillions;
-    public double shop4CostSexvigintillions;
-    public double shop4CostSeptenvigintillions;
-    public double shop4CostOctovigintillions;
-    public double shop4CostNovemvigintillions;
-
+    public List<double> shop4List = new List<double>();
 
     [Header("Church")]
     public Text shop5Text;
-
-    [Header("Church Cost")]
     public double shop5Price;
-
-    [Header("Church Ones - Nonillions")]
-    public double shop5CostOnesTensHundreds;
-    public double shop5CostThousands;
-    public double shop5CostMillions;
-    public double shop5CostBillions;
-    public double shop5CostTrillions;
-    public double shop5CostQuadrillons;
-    public double shop5CostQuintrillons;
-    public double shop5CostSextillons;
-    public double shop5CostSeptillons;
-    public double shop5CostOctillions;
-    public double shop5CostNonillions;
-
-    [Header("Church Decillions")]
-    public double shop5CostDecillions;
-    public double shop5CostUndecillions;
-    public double shop5CostDuodecillions;
-    public double shop5CostTredecillions;
-    public double shop5CostQuaddecillions;
-    public double shop5CostQuindecillions;
-    public double shop5CostSexdecillions;
-    public double shop5CostSeptdecillions;
-    public double shop5CostOctodecillions;
-    public double shop5CostNovemdecillions;
-
-    [Header("Church Vigintillions")]
-    public double shop5CostVigintillions;
-    public double shop5CostDuovigintillions;
-    public double shop5CostTresvigintillions;
-    public double shop5CostQuattorvigintillions;
-    public double shop5CostQuinvigintillions;
-    public double shop5CostSexvigintillions;
-    public double shop5CostSeptenvigintillions;
-    public double shop5CostOctovigintillions;
-    public double shop5CostNovemvigintillions;
-
+    public List<double> shop5List = new List<double>();
 
     // Buildings
     [Header("Buildings")]
@@ -381,52 +68,78 @@ public class Game : MonoBehaviour
     [Header("House")]
     public Int64 amount1;
     public Text amount1Text;
-
     public double amount1Upgrade;
+    public List<double> shop1UpgradeList = new List<double>();
     public double amount1Profit;
+    public List<double> shop1ProfitList = new List<double>();
+
     [Header("Tavern")]
     public Int64 amount2;
     public Text amount2Text;
-
     public double amount2Upgrade;
+    public List<double> shop2UpgradeList = new List<double>();
     public double amount2Profit;
+    public List<double> shop2ProfitList = new List<double>();
+
     [Header("Stable")]
     public Int64 amount3;
     public Text amount3Text;
-
     public double amount3Upgrade;
+    public List<double> shop3UpgradeList = new List<double>();
     public double amount3Profit;
+    public List<double> shop3ProfitList = new List<double>();
+
     [Header("Bakery")]
     public Int64 amount4;
     public Text amount4Text;
-
     public double amount4Upgrade;
+    public List<double> shop4UpgradeList = new List<double>();
     public double amount4Profit;
+    public List<double> shop4ProfitList = new List<double>();
+
     [Header("Church")]
     public Int64 amount5;
     public Text amount5Text;
-
     public double amount5Upgrade;
+    public List<double> shop5UpgradeList = new List<double>();
     public double amount5Profit;
+    public List<double> shop5ProfitList = new List<double>();
 
     // Upgrades
     [Header("Upgrades")]
 
     [Header("Non-building Upgrades")]
+
+    [Header("Overall Upgrades")]
     public double allUpgradePrice;
+    public List<double> allUpgradePriceList = new List<double>();
     public Text allUpgradeText;
+    [Header("Click Upgrades")]
     public double clickUpgradePrice;
+    public List<double> clickUpgradePriceList = new List<double>();
     public Text clickUpgradeText;
-    [Header("Buildings 1-5 Upgrades")]
+
+    [Header("Building Upgrades")]
+    
+    [Header("House Upgrades")]
     public double houseUpgradePrice;
+    public List<double> houseUpgradePriceList = new List<double>();
     public Text houseUpgradeText;
+    [Header("Tavern Upgrades")]
     public double tavernUpgradePrice;
+    public List<double> tavernUpgradePriceList = new List<double>();
     public Text tavernUpgradeText;
+    [Header("Stable Upgrades")]
     public double stableUpgradePrice;
+    public List<double> stableUpgradePriceList = new List<double>();
     public Text stableUpgradeText;
+    [Header("Bakery Upgrades")]
     public double bakeryUpgradePrice;
+    public List<double> bakeryUpgradePriceList = new List<double>();
     public Text bakeryUpgradeText;
+    [Header("Church Upgrades")]
     public double churchUpgradePrice;
+    public List<double> churchUpgradePriceList = new List<double>();
     public Text churchUpgradeText;
 
     // Achievements
@@ -589,9 +302,9 @@ public class Game : MonoBehaviour
         totalScore = totalScore + scoreIncreasedPerSecond;
 
         // Test
-        increaseOnesTensHundreds = (amount1Profit + amount2Profit + amount3Profit + amount4Profit + amount5Profit) * levelMultiplier * Time.deltaTime;
-        currentOnesTensHundreds = currentOnesTensHundreds + increaseOnesTensHundreds;
-        totalOnesTensHundreds = totalOnesTensHundreds + increaseOnesTensHundreds;
+        currentScoreList[0] = currentScoreList[0] + scoreIncreasedPerSecond;
+        scoreIncreaseList[0] = (amount1Profit + amount2Profit + amount3Profit + amount4Profit + amount5Profit) * levelMultiplier * Time.deltaTime;
+        totalScoreList[0] = totalScoreList[0] + scoreIncreaseList[0];
 
         // Shop
         shop1Text.text = "House: £" + shop1Price;
@@ -791,28 +504,32 @@ public class Game : MonoBehaviour
             goldCoin.transform.position = new UnityEngine.Vector3(UnityEngine.Random.Range(0, 400), UnityEngine.Random.Range(0, 400), 0);
             goldCoin.SetActive(true);
         }
+    
+        // TEST
 
-
-        // Test
-        totalOnesTensHundreds = totalOnesTensHundreds + scoreIncreasedPerSecond;
-        
-        if (totalOnesTensHundreds >= 1000)
+        if (totalScoreList[0] >= 1000)
         {
-            totalThousands++;
-            totalOnesTensHundreds = 0;
+            totalScoreList[1]++;
+            totalScoreList[0] = 0;
         }
         
 
-        if (totalThousands >= 1000)
+        if (totalScoreList[1] >= 1000)
         {
-            totalMillions++;
-            totalThousands = 0;
+            totalScoreList[2]++;
+            totalScoreList[1] = 0;
        
         }
-        if (totalMillions >= 1000)
+        if (totalScoreList[2] >= 1000)
         {
-            totalBillions++;
-            totalMillions = 0;
+            totalScoreList[3]++;
+            totalScoreList[2] = 0;
+        }
+
+        if (currentScoreList[0] >= 1000)
+        {
+            currentScoreList[1]++;
+            currentScoreList[0] = 0;
         }
     }
 
@@ -820,7 +537,8 @@ public class Game : MonoBehaviour
     public void Hit()
     {
         currentScore += hitPower * levelMultiplier;
-        totalScore += hitPower;
+        totalScore += hitPower * levelMultiplier;
+        totalScoreList[0] += hitPower * levelMultiplier;
         xp++;
 
         // Text
